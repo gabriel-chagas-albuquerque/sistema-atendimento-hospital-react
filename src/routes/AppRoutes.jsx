44187c61960514e36454291e68a629484afa4ab7
gabriel-chagas-albuquerque <gabrieldafortaleza0100@gmail.com>
2025-07-02 09:47:53 -0300

    Criação dos componentes Header e Footer, criação de Layout para replicar o Header e o Footer para todos os componentes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,20 +5,23 @@ import Triagem from "../pages/Triagem";
 import FilaEspera from "../pages/FilaEspera";
 import LoginMedico from "../pages/LoginMedico";
 import PainelMedico from "../pages/PainelMedico";
+import PageLayout from "../layouts/PageLayout";
 
 const AppRoutes = () => {
-    return ( 
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/paciente" element={<PainelPaciente />} />
-                <Route path="/paciente/triagem" element={<Triagem />} />
-                <Route path="/paciente/fila-de-espera" element={<FilaEspera />} />
-                <Route path="/login" element={<LoginMedico />} />
-                <Route path="/painel-do-medico" element={<PainelMedico />} />
-            </Routes>
-        </BrowserRouter>
-     );
-}
- 
-export default AppRoutes
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PageLayout />} >
+          <Route index element={<Home />} />
+          <Route path="/paciente" element={<PainelPaciente />} />
+          <Route path="/paciente/triagem" element={<Triagem />} />
+          <Route path="/paciente/fila-de-espera" element={<FilaEspera />} />
+          <Route path="/login" element={<LoginMedico />} />
+          <Route path="/painel-do-medico" element={<PainelMedico />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
