d70a4ec801122680a1c1b17b01096e430c130402
Lohan <hiellohan@gmail.com>
2025-07-17 12:39:34 -0300

    Adicionado estruturas de rotas para o painel do paciente e outras paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { Routes, Route } from "react-router-dom"
+import PainelDoMedico from "./pages/PainelMedico";
+import Home from "./pages/Home";
+import PainelPaciente from "./pages/PainelPaciente";
+import Cadastro from "./pages/Cadastro";
+import FilaEspera from "./pages/FilaEspera";
+import Triagem from "./pages/Triagem";
 import AppRoutes from "./routes/AppRoutes"
 import './App.css'
 
 
 function App() {
   return(
-    <>
-      <AppRoutes />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/painel-medico" element={<PainelDoMedico />} />
+      <Route path="/painel-paciente" element={<PainelPaciente />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/fila-espera" element={<FilaEspera />} />
+      <Route path="/triagem" element={<Triagem />} />
+    </Routes>
   )
 }
 
--- src/pages/PainelPaciente.jsx
+++ src/pages/PainelPaciente.jsx
@@ -1,8 +1,32 @@
+import { Link } from "react-router-dom";
 
 const PainelPaciente = () => {
     return ( 
         <>
-            <h1>Painel do Paciente</h1>
+        <div>
+      <h1>Portal do Paciente</h1>
+      <p>Sistema de Atendimento Hospitalar</p>
+
+      <img
+        id="foto"
+        src="https://www.sbie.com.br/wp-content/uploads/2017/08/1-humanizacao-hospitalar.jpg"
+        alt="Humanização Hospitalar"
+      />
+
+      <div className="menu-nav">
+        <h2>Escolha uma opção</h2>
+        <nav>
+          <ul id="menu">
+            <li><Link to="/paciente/cadastro">Cadastro</Link></li>
+            <li><Link to="/paciente/fila">Fila de Espera</Link></li>
+          </ul>
+        </nav>
+      </div>
+
+      <footer>
+        <Link to="/">Página Inicial</Link>
+      </footer>
+    </div>
         </>
      );
 }
